d077ec5c55145047fec802ec305c42b1d2a206a1
DEMIAN <demiangomez504@gmail.com>
2026-09-08 22:36:45 -0300

    Modelado basico clases de dominio

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        public int Id {  get; set; }
+        public string Codigo { get; set; } = string.Empty;
+        public string Nombre {  get; set; } = string.Empty;
+        public string Descripcion {  get; set; } = string.Empty;
+        public Marca MarcaProducto { get; set; } = new Marca();
+        public Categoria CategoriaProducto { get; set; } = new Categoria();
+        public float Precio { get; set; }
+    }
+}
---
+++ Dominio/Marca.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio
+{
+    public class Marca
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+    }
+}
