bdca244c91d0ea5383f6872f0eb322d8e1fbd7d2
Ezequiel <ezequiel.bernal44@gmail.com>
2026-09-13 16:08:32 -0300

    Creacion de frm VerMarcas: Grilla,  metodos,  funcionalidad Y  diseño: A medias para probar comandos

---
+++ TPWinForm_equipo0/FrmVerMarcas.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Dominio;
+using Negocio;
+
+
+namespace TPWinForm_equipo0
+{
+    public partial class FrmVerMarcas : Form
+    {
+        public FrmVerMarcas()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmVerMarcas_Load(object sender, EventArgs e)
+        {
+           cargarGrilla ();
+
+        }
+
+        private void cargarGrilla()
+        {
+            MarcaNegocio negocio = new MarcaNegocio();
+            try
+            {
+                List <Marca> lista = negocio.Listar();
+                dgvMarcas.DataSource = lista;
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show( "Error al cargar las marcas: " + ex.Message,
+                 "Error ## ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }   
+
+        }
+
+    }
+}
